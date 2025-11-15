10cb8fdbe2e788b1a9fa8c1104f47bc0a69f95ef
jakub1t <cozzrobic6@gmail.com>
2025-11-15 22:50:43 +0100

    PLCC, SROCC and some minor changes.

---
+++ proj/kadid10k_iqa.py
@@ -0,0 +1,14 @@
+import timeit
+import itertools
+import skimage
+import scipy
+import pandas as pd
+import numpy as np
+
+import measures as ms
+import sgessim
+
+
+
+
+
--- proj/nits_iqa.py
+++ proj/nits_iqa.py
@@ -1,11 +1,12 @@
 
+import timeit
 import itertools
 import skimage
+import scipy
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
 
-import other_measures as ms
+import measures as ms
 import sgessim
 
 """
@@ -46,6 +47,7 @@ def iterate_images(original_image, image_array):
     mse_list = []
     rmse_list = []
     psnr_list = []
+    ssim_list = []
     sg_essim_list = []
 
     mini = 1.0
@@ -56,7 +58,7 @@ def iterate_images(original_image, image_array):
         
         mse = np.round(ms.mse(original_image, image), 3)
         mse_list.append(mse)
-        print(f"MSE: {mse}")
+        # print(f"MSE: {mse}")
 
         rmse = np.round(ms.rmse(original_image, image), 3)
         rmse_list.append(rmse)
@@ -66,20 +68,26 @@ def iterate_images(original_image, image_array):
         psnr_list.append(psnr)
         # print(f"PSNR: {psnr}")
 
-        # print(f"SSIM: {ms.ssim(original_image, image)}")
-        # print(f"Skimage SSIM: {skimage.metrics.structural_similarity(original_image, image, channel_axis=2)}")
+        ssim = np.round(skimage.metrics.structural_similarity(original_image, image, channel_axis=2), 3)
+        ssim_list.append(ssim)
+        print(f"SSIM: {ssim}")
 
         sg_essim = np.round(sgessim.sg_essim(original_image, image), 3)
         sg_essim_list.append(sg_essim)
-        # print(f"SG-ESSIM: {sg_essim}")
+        print(f"SG-ESSIM: {sg_essim}")
 
         if sg_essim < mini:
             mini = sg_essim
             image_n = image_name
+        
+        print("\n")
 
-    print(f"Min SG-ESSIM: {mini} for image: {image_n}")
+    print(f"Min SG-ESSIM: {mini} for image: {image_n}\n")
 
-    return mse_list, rmse_list, psnr_list, sg_essim_list
+    return mse_list, rmse_list, psnr_list, ssim_list, sg_essim_list
+
+
+
 
 
 dataframe = pd.read_excel("./images/nits_iqa/Database/Score.xlsx", sheet_name="Sheet1")
@@ -87,12 +95,12 @@ dataframe = dataframe.sort_values(by=["Original Image Name", "Distorted Image Na
 dataframe = dataframe.reset_index(drop=True)
 
 
-# all_deformed_images = []
 original_images = []
 NUMBER_OF_ORIGINAL_IMAGES = 9
 mse_values = []
 rmse_values = [] 
 psnr_values = [] 
+ssim_values = []
 sg_essim_values = []
 
 
@@ -101,18 +109,37 @@ for i in range(1, NUMBER_OF_ORIGINAL_IMAGES + 1):
     print(f"\nOriginal image no. {i} shape: {original_image.shape}\n")
     original_images.append(original_image)
 
+
+
+time_start = timeit.default_timer()
+
 for j in range(1, NUMBER_OF_ORIGINAL_IMAGES + 1):
     print(f"Collection no.: {j}")
     image_collection = skimage.io.imread_collection(f"./images/nits_iqa/Database/I{j}D*.bmp", conserve_memory=True)
-    # all_deformed_images.append(image_collection)
 
-    mse_v, rmse_v, psnr_v, sg_essim_v = iterate_images(original_images[j - 1], image_collection)
+    mse_v, rmse_v, psnr_v, ssim_v, sg_essim_v = iterate_images(original_images[j - 1], image_collection)
     mse_values.extend(mse_v)
     rmse_values.extend(rmse_v)
     psnr_values.extend(psnr_v)
+    ssim_values.extend(ssim_v)
     sg_essim_values.extend(sg_essim_v)
 
+time_end = timeit.default_timer()
+print(f"Time elapsed for processing: {time_end - time_start:.2f} seconds\n")
 
+
+
+pearson_coefficient, pe_p_value = scipy.stats.pearsonr(ssim_values, sg_essim_values)
+
+pearson_coefficient = np.round(pearson_coefficient, 3)
+
+spearman_coefficient, sp_p_value = scipy.stats.spearmanr(ssim_values, sg_essim_values)
+
+spearman_coefficient = np.round(spearman_coefficient, 3)
+
+print(f"\n===================\nPLCC: {pearson_coefficient}\n===================\n")
+print(f"\n===================\nSROCC: {spearman_coefficient}\n===================\n")
+print(f"\n===================\nKROCC: \n===================\n")
 
 # dataframe.style.set_properties(**{"background-color": "red"}, subset=["mse", "rmse", "psnr", "sg_essim"])
 
