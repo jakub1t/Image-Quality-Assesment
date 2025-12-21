28f4fdeba2bdad77fe740c4a706977f52bfb3162
jakub1t <cozzrobic6@gmail.com>
2025-12-21 02:01:03 +0100

    Attempt to add another db.

--- proj/main.py
+++ proj/main.py
@@ -2,24 +2,27 @@
 
 from nits_db import NITS_DB
 from kadid10k_db import KADID10K_DB
+from csiq_db import CSIQ_DB
 
 
 def main():
     print("Main script started...\n")
 
-    db = NITS_DB("database")
-    db.load_images_and_data()
-    db.perform_iqa("result_nits_iqa")
-
-    # db.load_reference_images()
-    # db.load_deformed_image_collections()
-    # db.calculate_quality_values()
+    # db = NITS_DB("database")
+    # db.load_images_and_data()
+    # db.perform_iqa("result_nits_iqa")
 
 
     # db = KADID10K_DB("database")
     # db.load_images_and_data()
     # db.perform_iqa("result_kadid10k_iqa")
     # db.read_image_data()
+
+
+    # db = CSIQ_DB("database")
+    # db.read_image_data()
+    # db.load_reference_images()
+    # db.load_deformed_image_collections()
 
 
     # ref_image = imread("./images/nits_iqa/Database/I1.bmp")
